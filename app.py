b73f006bd9c44631f080f02babf0e089d57a5210
Felipe Grandón <167049608+pipe-z3@users.noreply.github.com>
2025-07-11 15:23:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,6 @@ def index():
 
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+
